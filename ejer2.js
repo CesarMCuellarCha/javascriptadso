2f6ad019892994cbc0ff66158e58b8e29e588c55
CesarMCuellarCha <cesarmcuellar@gmail.com>
2025-08-04 11:42:15 -0500

    primer commit 3 ejercicios

---
+++ ejer2.js
@@ -0,0 +1,29 @@
+let ciudad="Popayán"
+
+
+function obtenerCiudad(){
+    let ciudad="Cali.."
+    console.log(ciudad)
+    ciudad="NeivaYork"
+    console.log(ciudad)
+    return ciudad
+}
+
+
+if (true){
+    let ciudad="asdadasd"
+
+}
+
+console.log(ciudad)
+console.log(obtenerCiudad())
+
+
+//comentario de una línea
+
+/**
+ * dasdasdasd
+ * dasdasdas
+ * dasdasd
+ * asdasdas
+ */
---
+++ ejer3.js
@@ -0,0 +1,14 @@
+let prueba
+
+prueba="mensaje"
+
+
+const carro = {
+    "placa": "xxx123",
+    "marca": "Renault"
+}
+
+console.log(carro)
+carro.placa="ZZZ456"
+
+console.log(carro)
